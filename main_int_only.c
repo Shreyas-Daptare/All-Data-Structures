1d76c8f5633a5383abc47d0eac21a5909d981137
Shreyas Daptare <116744812+Shreyas-Daptare@users.noreply.github.com>
2022-12-14 09:05:01 +0530

    Update main_int_only.c

--- main_int_only.c
+++ main_int_only.c
@@ -291,8 +291,96 @@ void radix_sort()
       printf ("%d ", a[i]);
    printf ("\n");	
 }
+
+void quick_sort()
+{
+	printf("Quick Sort \n\n");
+	printf("-----------------------------------------------------------------------------------------------------------------\n");
+	int data[100],i,j,n;
+   //Functions
+   // function to swap elements
+	void swap(int *a, int *b) {
+ 	 int t = *a;
+  	 *a = *b;
+  	 *b = t;
+}
+
+// function to find the partition position
+	int partition(int array[], int low, int high) {
+  
+ 	 // select the rightmost element as pivot
+	 int pivot = array[high];
+  
+  // pointer for greater element
+ 	 int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+        
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+      
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+  
+  // return the partition point
+  return (i + 1);
+}
+
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    
+    // find the pivot element such that
+    // elements smaller than pivot are on left of pivot
+    // elements greater than pivot are on right of pivot
+    int pi = partition(array, low, high);
+    
+    // recursive call on the left of pivot
+    quickSort(array, low, pi - 1);
+    
+    // recursive call on the right of pivot
+    quickSort(array, pi + 1, high);
+  }
+}
+
+// function to print array elements
+void printArray(int array[], int size) {
+  for (i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+    printf("Enter the size of the array:");
+	scanf("%d", &n);
+	
+	printf("\nEnter the %d elements:\n", n);
+	for(i=0;i<n;i++)
+	{
+		printf("Enter element %d:", i+1);
+		scanf("%d", &data[i]);	
+	}
+	
+	printf("\nEntered elements before sorting:\n");
+	printArray(data, n);
+  
+  // perform quicksort on data
+    quickSort(data, 0, n - 1);
+  
+    printf("Sorted array in ascending order: \n");
+    printArray(data, n);
+}
 int main()
 {
+	//gotolabel
+	main_menu:
     printf("-----------------------------------------------------------------------------------------------------------------\n");
     printf("-----------------------------------------------------------------------------------------------------------------\n\n");
     printf("                                   Implement all types of Data Structures\n\n");
@@ -332,6 +420,8 @@ int main()
 							break;
 					case 3:
 						printf("Exiting to Main Menu...");
+						clear();
+						goto main_menu;
 						break;
 				}
             break;
@@ -370,10 +460,13 @@ int main()
 					case 5: 
 							//Quick sort
 							clear();
+							quick_sort();
 							break;
 					case 6:
-						printf("Exiting to Main Menu...");
-						break;
+							printf("Exiting to Main Menu...");
+							clear();
+							goto main_menu;
+							break;
 				}
             break;
         case 3:
@@ -382,7 +475,33 @@ int main()
             printf("2: Stack\n");
             printf("3: Queue\n");
             printf("4: Linked List\n");
-            printf("5: Quick Sort\n");
+            printf("5: Return to Main Menu \n\n");
+            printf("Choice: ");
+            scanf("%d",&sub_choice);
+    		switch(sub_choice)
+    			{
+    				case 1:
+							clear();
+							//array();
+							break;
+					case 2:
+							clear();
+							//stack();
+							break;
+					case 3:
+							clear();
+							//queue();
+							break;
+					case 4:
+							clear();
+							//linked_list();
+							break;
+					case 5:
+							printf("Exiting to Main Menu...");
+							clear();
+							goto main_menu;
+							break;
+				}
             break;
         case 4:
             printf("  Non-Linear Data Structures\n");
